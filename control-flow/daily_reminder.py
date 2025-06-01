e678ad783a3c4d82b45b66e7dac8e2bab1c1837e
Ali Osama <122614509+itsali10@users.noreply.github.com>
2025-06-01 19:59:42 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,23 +5,23 @@ time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
         if time_bound == "yes":
-            print (f"'{task}' is a high priority task that requires immediate attention today!")
+            print (f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"'{task}' is a high priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
         else:
             print("Time-bound is either yes or no")
     case "medium":
         if time_bound == "yes":
-            print (f"'{task}' is a medium priority task that requires immediate attention today!")
+            print (f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"'{task}' is a medium priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
         else:
             print("Time-bound is either yes or no")
     case "low":
         if time_bound == "yes":
-            print (f"'{task}' is a low priority task that requires immediate attention today!")
+            print (f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
         elif time_bound == "no":
-            print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
         else:
             print("Time-bound is either yes or no")
     case _:
