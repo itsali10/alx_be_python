40197a57efea0ebc39d2909c4ee518c06e881671
itsali10 <itsali8777@gmail.com>
2025-06-01 15:59:45 +0300

    Task 1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1=10
+number2=5
+
+print(f"Addition of {number2} and {number1} is {number1 + number2}")
+print(f"Subtraction of {number2} and {number1} is {number2 - number1}")
+print(f"Multiplication of {number2} and {number1} is {number1 * number2}")
