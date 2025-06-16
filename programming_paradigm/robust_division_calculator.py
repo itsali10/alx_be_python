938b6569ed5674659113ff0f5e47431e94381475
Ali Osama <122614509+itsali10@users.noreply.github.com>
2025-06-16 15:11:53 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -12,7 +12,7 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
 
     except ValueError:
-        return "Error: Both inputs must be numeric."
+        return "Error: Please enter numeric values only."
     
 def main():
     if len(sys.argv) != 3:
